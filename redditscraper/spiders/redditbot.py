734e32402a75e2bdb340cc338709eb6579ff5267
JanmejaiPandey <pandey.janmejai@gmail.com>
2019-06-20 10:35:00 +0530

    postedby update

--- redditscraper/spiders/redditbot.py
+++ redditscraper/spiders/redditbot.py
@@ -10,13 +10,15 @@ class RedditbotSpider(scrapy.Spider):
     def parse(self, response):
 
         for item in response.css("div.Post"):
-            titles = item.css("h2::text").get()
+            titles = item.css("._eYtD2XCVieq6emjKBH3m::text").get()
             votes = item.css("._1rZYMD_4xY3gRcSS3p8ODO::text").get()
             times = item.css("._3jOxDPIQ0KaOWpzvSQo-1s::text").get()
+            postedby = item.css("._2tbHP6ZydRpjI44J3syuqC._23wugcdiaj44hdfugIAlnX.oQctV4n0yUb0uiHDdGnmE::text").get()
             comments = item.css(".FHCV02u6Cp2zYL0fhQPsO::text").get()
             yield {
                 'title':titles,
                 'vote':votes,
                 'time': times,
                 'comment':comments,
+                'postedby':postedby,
             }
